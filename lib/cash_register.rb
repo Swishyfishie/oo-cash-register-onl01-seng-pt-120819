4d8060d73a13ed4b96f0ad03bb9cee0965a678a7
Andrei Pestenariu <pclandrei@gmail.com>
2020-01-03 00:53:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   def add_item(title, price, quantity = 1) 
     quantity == 1 ? self.total+= price : self.total += price * quantity
     @items.fill(title, @items.length , quantity)
-    p @last_transaction
+    @total = @total
   end 
     
   def apply_discount
